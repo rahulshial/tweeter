8ab1dfea4717372169ecbecbc17ad74dd9b21da2
Rahul <rahul.shial@gmail.com>
2020-11-18 22:11:16 +0000

    Dynamic Tweets updated the days ago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,44 +8,43 @@ $(document).ready(function() {
 
   const createTweetElement = function(record) {
 
-    // const $tweet = $(`<article class="tweet-hello-world">Hello world</article>`);
-    //create tweetMarkup
-    const $$userName = record['user']['name'];
-    const $avatars = record['user']['avatars'];
-    const $userHandle = record['user']['handle'];
-    const $tweetBody = record['content']['text'];
-    const $createdAt = record['created_at'];
+    const userName = record['user']['name'];
+    const avatars = record['user']['avatars'];
+    const userHandle = record['user']['handle'];
+    const tweetBody = record['content']['text'];
+    let createdAt = Math.round((Date.now() - record['created_at']) / 86400000);
+    (createdAt > 1 ? createdAt += ' days ' : createdAt += ' day ');
 
-    const $tweetMarkUp = `
-    <div class="tweet-list">
-    <!-- have a header, which contains the user's: avatar, then name, and handle on extreme right -->
-    <header class="tweet-list-header">
-      <div class="avatar-name-wrapper">
-        <img class="avatar" src="${$avatars}"></img>
-        <label name="user-name" class="user-name">${$$userName}</label>
+    const tweetMarkUp = `
+      <div class="tweet-list">
+        <!-- have a header, which contains the user's: avatar, then name, and handle on extreme right -->
+        <header class="tweet-list-header">
+          <div class="avatar-name-wrapper">
+            <img class="avatar" src="${avatars}"></img>
+            <label name="user-name" class="user-name">${userName}</label>
+          </div>
+          <div>
+            <label class="user-handle">${userHandle}</label>
+          </div>
+        </header>
+        <!-- have a body, which contains the tweet text -->
+        <div class="tweet-list-body">
+          <p>${tweetBody}</p>
+        </div>
+        <!-- have a footer which displays: how long ago tweet was created on the left, and "Flag", "Re-tweet" and "Like" icons upon hovering over the tweet, on the right -->
+        <footer class="tweet-list-footer">
+          <div class="created-on">
+            <p>${createdAt} ago</p>
+          </div>
+          <div class="icons">
+            <i class="fa fa-flag"></i>
+            <i class="fa fa-heart"></i>
+            <i class="fa fa-retweet"></i>
+          </div>
+        </footer>
       </div>
-      <div>
-        <label class="user-handle">${$userHandle}</label>
-      </div>
-    </header>
-    <!-- have a body, which contains the tweet text -->
-    <div class="tweet-list-body">
-      <p>${$tweetBody}</p>
-    </div>
-    <!-- have a footer which displays: how long ago tweet was created on the left, and "Flag", "Re-tweet" and "Like" icons upon hovering over the tweet, on the right -->
-    <footer class="tweet-list-footer">
-      <div class="created-on">
-        <p>${$createdAt}</p>
-      </div>
-      <div class="icons">
-        <i class="fa fa-flag"></i>
-        <i class="fa fa-heart"></i>
-        <i class="fa fa-retweet"></i>
-      </div>
-    </footer>
-    </div>
     `;
-    return $tweetMarkUp;
+    return tweetMarkUp;
   };
 
   // Test / driver code (temporary). Eventually will get this from the server.
@@ -85,8 +84,6 @@ $(document).ready(function() {
 
   // Test / driver code (temporary)
   renderTweets();
-  // console.log($tweet); // to see what it looks like
-  // $('.tweet-list').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 });
 
 
